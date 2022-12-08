c6f4057b08273755163d871fb7b7a487efd79bc6
yook <yhj03069@gmail.com>
2022-12-08 10:19:53 +0900

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by yook on 2022/12/08.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
