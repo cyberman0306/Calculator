32236c177e9a16a3c3d702c190bacab502160198
HYUN JUN YOOK <yhj03069@gmail.com>
2023-01-27 16:22:30 +0900

    기초 화면

--- calculator/CalculatorBrain.swift
+++ calculator/CalculatorBrain.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+
+class CalculatorModel: ObservableObject {
+    
+    @Published var displayValue: String = "0"
+    
+    private let buttonCodeVertical: [[String]] = [
+        ["C", "±", "%", "÷"],
+        ["7", "8", "9", "x"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ",", "="],
+]
+    private let buttonCodeHorizontal: [[String]] = [
+        ["(",   ")",    "mc",   "m+",   "m-",   "mr",   "C",    "±",    "%",    "÷"],
+        ["2nd", "x²",   "x³",   "xʸ",   "eˣ",   "10ˣ",  "7",    "8",    "9",    "×"],
+        ["√x",  "2/x",  "3/x",  "y/x",  "ln",   "log10","4",    "5",    "6",    "−"],
+        ["x!",  "sin",  "cos",  "tan",  "e",    "EE",   "1",    "2",    "3",    "+"],
+        ["Rad", "sinh", "cosh", "tanh", "pi",   "Rand", "0",    "0",    ".",    "="]
+    ]
+    
+    func getButtonCodeList() -> [[String]] {
+        return buttonCodeVertical
+    }
+    
+    func inputToken(input:String) {
+        if let _ = Int(input) {
+            inputDigit(input: input)
+        }
+        else {
+            if userIsInTheMiddleOfTyping, let value =
+                Double(displayValue) {
+                setOperand(operand: value)
+                userIsInTheMiddleOfTyping = false
+            }
+            performOperation(input)
+            displayValue = String(result)
+        }
+    }
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    private func inputDigit(input: String) {
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = displayValue
+            displayValue = textCurrentlyInDisplay + input
+        } else {
+            displayValue = input
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    private var accumulator: Double?
+    
+    private func performOperation(_ symbol:String) {
+        userIsInTheMiddleOfTyping = false
+    }
+    
+    private func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    private var result: Double {
+        get {
+            return accumulator!
+        }
+    }
+    
+    
+    
+}
--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -7,114 +7,17 @@
 
 import SwiftUI
 
-private let buttonCodeVertical: [[String]] = [
-    ["C", "±", "%", "÷"],
-    ["7", "8", "9", "x"],
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["0", "0", ",", "="],
-
-]
-enum Buttontype {
-    case first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, zero
-    case dot, equal, plus, minus, multiple, devide
-    case percent, opposite, clear
-    
-    var ButtonDisplayname: String {
-        switch self {
-        case .first :
-            return "1"
-        case .second :
-            return "2"
-        case .third :
-            return "3"
-        case .fourth :
-            return "4"
-        case .fifth :
-            return "5"
-        case .sixth :
-            return "6"
-        case .seventh :
-            return "7"
-        case .eighth :
-            return "8"
-        case .nineth :
-            return "9"
-        case .zero :
-            return "0"
-        case .equal :
-            return "="
-        case .plus :
-            return "+"
-        case .minus :
-            return "-"
-        case .multiple :
-            return "X"
-        case .devide :
-            return "/"
-        case .percent :
-            return "%"
-        case .opposite :
-            return "+/-"
-        case .clear :
-            return "A/C"
-        case .dot :
-            return "."
-        default:
-            return "?"
-        }
-    }
-    
-    var backgroundColor: Color {
-        switch self {
-        case .first, .second, .third, .fourth, .fifth, .sixth, .seventh,
-                .eighth, .nineth, .zero, .dot:
-            return Color("NumberButton")
-        case .equal, .plus, .minus, .multiple, .devide:
-            return Color.orange
-        case .percent, .opposite, .clear:
-            return Color.gray
-        }
-    }
-    
-    var forgroundColor: Color {
-        switch self {
-        case .first, .second, .third, .fourth, .fifth, .sixth, .seventh,
-                .eighth, .nineth, .zero, .dot, .equal, .plus, .minus, .multiple, .devide:
-            return Color.white
-        case .percent, .opposite, .clear:
-            return Color.black
-        }
-    }
-}
-
 struct ContentView: View {
+    
+    @ObservedObject private var brain: CalculatorModel = CalculatorModel()
+    
     @State private var result: String = "0"
     
     func inputToken(token: String) {
-        result += token
+        brain.inputToken(input: token)
     }
     
     
-    @State private var totalNumber: String = "0"
-    @State private var PreviousResult: String = "0" //결과값 저장
-    @State private var RealtimeResult: String = "0" //결과값 저장
-    @State var tempNumber: Int = 0 // 계산용
-    @State var first_Number: Int = 0 //첫 입력 저장
-    @State var after_Number: Int = 0 //나중 입력 저장
-    @State var operatorType: Buttontype = .clear
-    @State var ItisEmpty: Bool = true
-    @State var first_data_exist: Bool = false
-    @State var after_data_exist: Bool = false
-    
-    
-    private let buttonData: [[Buttontype]] = [
-        [.clear, .opposite, .percent, .devide],
-        [.seventh, .eighth, .nineth, .multiple],
-        [.fourth, .fifth, .sixth, .minus],
-        [.first, .second, .third, .plus],
-        [.zero, .dot, .equal]
-    ]
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
@@ -122,30 +25,17 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Text(result)
+                    Text(brain.displayValue)
                         .padding()
                         .font(.system(size: 73))
                         .foregroundColor(.white)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
                 }
                 
                 
-                // Leeo version 230126
-//                HStack {
-//                    Spacer()
-//                    Text(RealtimeResult)
-//                        .padding()
-//                        .font(.system(size: 53))
-//                        .foregroundColor(.white)
-//                }
-//                HStack{
-//                    Spacer()
-//                    Text(totalNumber)
-//                        .padding()
-//                        .font(.system(size: 73))
-//                        .foregroundColor(.white)
-//                }
                 
-                ForEach(Array(buttonCodeVertical.enumerated()), id: \.offset) { vIdx, line in
+                ForEach(Array(brain.getButtonCodeList().enumerated()), id: \.offset) { vIdx, line in
                     HStack {
                         let hLast = line.count - 1
                         ForEach(Array(line.enumerated()), id: \.offset) { hIdx, buttonTitle in
@@ -163,120 +53,11 @@ struct ContentView: View {
                 }
                 .padding()
                 
-                // Leeo version 230126
-//                ForEach(buttonData, id: \.self)  { line in
-//                    HStack {
-//                        ForEach(line, id: \.self) { item in
-//                            Button {
-//                                if ItisEmpty { // 현재 입력 공간에 아무것도 없을때
-//                                    if item == .clear ||
-//                                        item == .plus ||
-//                                        item == .minus ||
-//                                        item == .multiple ||
-//                                        item == .devide ||
-//                                        item == .opposite ||
-//                                        item == .percent ||
-//                                        item == .equal { // 입력없이 연산자를 누를떄 무반응
-//                                        ItisEmpty = true
-//                                    } else { // 첫번쨰 숫자 저장 및 디스플레이
-//                                        ItisEmpty = false
-//                                        if operatorType != .clear {
-//                                            totalNumber = item.ButtonDisplayname
-//                                            after_Number = Int(totalNumber) ?? 0
-//                                            //
-//                                            if operatorType == .plus {
-//                                                RealtimeResult = String(first_Number + after_Number)
-//                                            } else if operatorType == .multiple {
-//                                                RealtimeResult = String(first_Number * after_Number)
-//                                            } else if operatorType == .minus {
-//                                                RealtimeResult = String(first_Number - after_Number)
-//                                            }
-//                                            //
-//                                        } else {
-//                                            totalNumber = item.ButtonDisplayname
-//                                        }
-//                                    }
-//                                } else { // 두번째 입력
-//                                    if item == .clear {
-//                                        first_Number = 0
-//                                        totalNumber = "0"
-//                                        after_Number = 0
-//                                        PreviousResult = "0"
-//                                        RealtimeResult = "0"
-//                                        ItisEmpty = true
-//                                    } else if item == .plus {
-//                                        first_Number = Int(totalNumber) ?? 0
-//                                        operatorType = .plus
-//                                        totalNumber += "+"
-//                                        ItisEmpty = true
-//                                        print(first_Number)
-//                                    } else if item == .multiple {
-//                                        first_Number = Int(totalNumber) ?? 0
-//                                        operatorType = .multiple
-//                                        totalNumber += "X"
-//                                        ItisEmpty = true
-//                                    } else if item == .minus {
-//                                        first_Number = Int(totalNumber) ?? 0
-//                                        operatorType = .minus
-//                                        totalNumber += "-"
-//                                        ItisEmpty = true
-//                                    } else if item == .opposite {
-//                                        print(totalNumber)
-//                                        tempNumber = Int(totalNumber) ?? 0
-//                                        tempNumber *= -1
-//                                        print(tempNumber)
-//                                        totalNumber = String(tempNumber)
-//                                    }
-//
-//                                    else if item == .equal {
-//                                          if operatorType == .plus ||
-//                                             operatorType == .multiple ||
-//                                             operatorType == .minus {
-//                                              totalNumber = RealtimeResult
-//                                              RealtimeResult = "0"
-//                                          }
-//                                        first_Number = Int(totalNumber) ?? 0
-//                                        PreviousResult = totalNumber
-//                                        }
-//
-//                                       else {
-//                                           totalNumber += item.ButtonDisplayname
-//                                           if operatorType != .clear {
-//                                               after_Number = Int(totalNumber) ?? 0
-//                                           }
-//
-//                                       }
-//                                }
-//                            } label: {
-//                                Text(item.ButtonDisplayname)
-//                                    .frame(width: calculateButtonWidth(button: item),
-//                                           height: calculateButtonHeight(button: item))
-//                                    .background(item.backgroundColor)
-//                                    .cornerRadius(40)
-//                                    .foregroundColor(item.forgroundColor)
-//                                    .font(.system(size: 33))
-//                            }
-//                        }
-//                    }
-//                }
-                
             }
         }
        
     }
     
-    private func calculateButtonWidth(button buttontype: Buttontype) -> CGFloat {
-        switch buttontype {
-        case .zero:
-            return (UIScreen.main.bounds.width - 5 * 10) / 4 * 2
-        default:
-            return ((UIScreen.main.bounds.width - 5 * 10) / 4 )
-        }
-    }
-    
-    private func calculateButtonHeight(button: Buttontype) -> CGFloat {
-        return (UIScreen.main.bounds.width - 5 * 10 ) / 4
-    }
     
 }
 
